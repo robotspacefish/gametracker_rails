b75e1fcf78f14f280d55e81f226ba118a6e30e3e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 15:09:22 -0500

    index action

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,10 @@
 class CollectionsController < ApplicationController
+
+  def index
+    if params[:user_id] && user = User.find_by(id: params[:user_id])
+      @collections = user.collections
+    else
+      # redirect?
+    end
+  end
 end
