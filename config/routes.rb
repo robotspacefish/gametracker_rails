68e0ff35ac2b211eb071ec5ef7e92d21aed88ac2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 15:09:38 -0500

    nest collections#index under users

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # resources :images
   # resources :platforms
   # resources :games
+
+  resources :users do
+    resources :collections, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
