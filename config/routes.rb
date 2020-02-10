e7eba85d5335d47dbdd79fc303b5b49d8a65ea27
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 14:03:33 -0500

    comment out route resources for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :genres
-  resources :images
-  resources :platforms
-  resources :games
+  # resources :notes
+  # resources :genres
+  # resources :images
+  # resources :platforms
+  # resources :games
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
