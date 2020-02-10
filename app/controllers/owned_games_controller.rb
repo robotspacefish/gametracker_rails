ca02644d2befae39c55749e8c4d82b3cdc2c00f0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 18:40:32 -0500

    use games_platforms instead of owned_games

--- app/controllers/owned_games_controller.rb
+++ app/controllers/owned_games_controller.rb
@@ -1,9 +1,8 @@
 class OwnedGamesController < ApplicationController
 
   def index
-    binding.pry
     if params[:user_id] && user = User.find_by(id: params[:user_id])
-      @owned_games = user.owned_games
+      @owned_games = user.games_platforms
     else
       # redirect?
     end
