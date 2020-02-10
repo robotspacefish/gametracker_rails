da8f4573c89f97dd5a49220f9eb7356107d6b54c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 18:50:18 -0500

    validate username for uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :username, presence: true, uniqueness: true
+
   has_many :owned_games
   has_many :games_platforms, through: :owned_games
 end
