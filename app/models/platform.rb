f4966c863f39a23bca94791190578618b5a2c626
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 18:56:37 -0500

    validate platform name for uniqueness (not case sensitive) and presence

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,4 +1,6 @@
 class Platform < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
   has_many :games_platforms
   has_many :games, through: :games_platforms
 end
