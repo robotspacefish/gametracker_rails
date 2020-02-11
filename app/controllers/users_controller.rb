210a941ef6ba68f7d92337d46990553698073b4f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 19:24:02 -0500

    add password_conf to user_params & create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_collection_path(@user) #todo change
+    else
+      render :new
+    end
+  end
+
   private
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
