c1dd6b97b7690fca42f824cf2aa4e5e72f0d2e5b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 20:35:25 -0500

    create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,17 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_collection_path(user)
+    else
+      flash[:message] = "Incorrect login information. Please try again"
+      redirect_to login_path
+    end
+  end
+
 end
