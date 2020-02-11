9d70fbdf2730c7292f54056d3dafb38b85de4b4d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 19:29:27 -0500

    redirect if not logged in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     def logged_in?
       !!current_user
     end
+
+    def redirect_if_not_logged_in
+      redirect_to root_path if !logged_in?
+    end
 end
