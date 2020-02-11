5b0bb15fc88845480851427a9d7c3c25d0d9806a
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-11 12:25:13 -0500

    user account nested route

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,10 @@ Rails.application.routes.draw do
 
   resources :games
 
-  resources :users do
+  resources :users, only: [:index, :new, :create, :show] do
     # resources :collections, only: [:index]
     get '/collection', to: 'owned_games#index'
+    get '/account', to: 'accounts#index'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
