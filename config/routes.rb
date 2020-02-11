bd6f7c7b048982ff9f3df5d6dcc3b530ac6a5231
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 21:06:46 -0500

    home and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # resources :notes
   # resources :genres
