5b596d44d65393f8e8e9250b52642ec4d8d5df7c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-11 11:45:51 -0500

    add has many games association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
   has_many :owned_games
   has_many :games_platforms, through: :owned_games
-
-   scope :sort_by_username, -> { order(:username) }
+  has_many :games, through: :games_platforms
+  scope :sort_by_username, -> { order(:username) }
 end
