8e68f74d740f5015a11b5fb6113a362daea57312
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 19:06:28 -0500

    get and post signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   # resources :notes
   # resources :genres
   # resources :images
   # resources :platforms
+
 
   resources :games
 
