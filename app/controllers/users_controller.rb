8a958d2c6f79a82a5f8996f5efbd3691d07894cf
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 19:08:00 -0500

    user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
