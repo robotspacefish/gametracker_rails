9a96ab89a0f8a5ca34e6461a6157c038a8ef44ae
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-10 20:53:44 -0500

    make current_user and logged_in? helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
   private
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
