046064a5f0b6adba8fc4585e65f7548a4ee909a2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-11 17:16:29 -0500

    fix user should be @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to user_collection_path(@user) #todo change
     else
       render :new
