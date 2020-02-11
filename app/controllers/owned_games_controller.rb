3f4bef81ce56f09774bdcdd9ce563b4fbd0c17a4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-11 11:50:03 -0500

    use user.games.distinct to get unique game titles owned by user

--- app/controllers/owned_games_controller.rb
+++ app/controllers/owned_games_controller.rb
@@ -4,7 +4,9 @@ class OwnedGamesController < ApplicationController
   def index
     user = User.find_by(id: params[:user_id])
     if params[:user_id] && user && is_current_user?(params[:user_id])
-      @owned_games = user.games_platforms
+      # @owned_games = user.games_platforms
+      @owned_games = user.games.distinct
+
     else
       redirect_to games_path
     end
