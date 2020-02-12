ca2870d96dcb52e57a94455b2c4546e3d78615d8
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-12 16:49:14 -0500

    change belongs to games_platforms to belongs_to games

--- db/migrate/20200210213808_create_owned_games.rb
+++ db/migrate/20200210213808_create_owned_games.rb
@@ -2,7 +2,7 @@ class CreateOwnedGames < ActiveRecord::Migration[6.0]
   def change
     create_table :owned_games do |t|
       t.belongs_to :user
-      t.belongs_to :games_platform
+      t.belongs_to :games
       t.boolean :currently_playing
       t.boolean :want_to_play
       t.boolean :completed
