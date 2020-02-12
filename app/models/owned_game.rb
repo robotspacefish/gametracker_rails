0c2ef8ed6bec70972198b924d1b322a0ff0ac822
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-12 16:42:17 -0500

    create owned game from params
    create GamePlatform instances for each platform id and create OwnedGames for each platform

--- app/models/owned_game.rb
+++ app/models/owned_game.rb
@@ -3,4 +3,19 @@ class OwnedGame < ApplicationRecord
   belongs_to :games_platform
 
   has_many :notes
+
+  def self.create_owned_game_from_params(params)
+    gps = params[:platform_ids].filter { |p_id| !p_id.blank? }.collect do |p_id|
+      GamesPlatform.find_by_ids(params[:game_id], p_id)
+
+    if game && !gps.empty?
+      return gps.collect do |gp|
+        OwnedGame.new(games_platform_id: gp.id,  completed: params["completed"],
+      currently_playing: params["currently_playing"],
+      want_to_play: params["want_to_play"] )
+      end
+    else
+      return nil
+    end
+  end
 end
