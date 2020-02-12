f7a44283fbb661071e21b1e42dd75b0c4650225c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-12 14:13:36 -0500

    routes to add a game to user's owned_games

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,15 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
 
   resources :notes, only: [:new, :create, :edit, :delete]
+
   resources :games
 
   resources :users do
+    # resources :owned_games, only: [:index, :create], as: 'collection'
     get '/collection', to: 'owned_games#index'
+    get '/collection/add_game', to: 'owned_games#new'
+    post '/collection', to: 'owned_games#create'
+
     get '/account', to: 'users#edit'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
