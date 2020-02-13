d59e4b27e347e28bcc1354a5577d7654cc6b2c62
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-12 22:23:08 -0500

    create owned games from form params

--- app/models/owned_game.rb
+++ app/models/owned_game.rb
@@ -4,18 +4,27 @@ class OwnedGame < ApplicationRecord
 
   has_many :notes
 
-  def self.create_owned_game_from_params(params)
+  def self.create_owned_games_from_params(params, user_id)
     gps = params[:platform_ids].filter { |p_id| !p_id.blank? }.collect do |p_id|
       GamesPlatform.find_by_ids(params[:game_id], p_id)
-
-    if game && !gps.empty?
-      return gps.collect do |gp|
-        OwnedGame.new(games_platform_id: gp.id,  completed: params["completed"],
-      currently_playing: params["currently_playing"],
-      want_to_play: params["want_to_play"] )
-      end
-    else
-      return nil
     end
+
+    return false if gps.empty?
+
+    gps.collect do |gp|
+      og = OwnedGame.new(
+        user_id: user_id,
+        game_id: params["game_id"],
+        completed: params["completed"],
+        currently_playing: params["currently_playing"],
+        want_to_play: params["want_to_play"]
+      )
+
+      ogs = OwnedGamesPlatform.new(user_id: user_id, games_platform_id: gp.id)
+
+      og.save && ogs.save
+    end
+
+    gps.all?
   end
 end
