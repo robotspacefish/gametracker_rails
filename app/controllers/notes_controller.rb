ce875c43d7cebe0022d7672596672425a3398f98
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-13 16:36:30 -0500

    notes params and new action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,19 @@
 class NotesController < ApplicationController
   before_action :redirect_if_not_logged_in
+
+  def new
+    # find owned game by game_id
+    @game = Game.find(params[:game_id])
+    if owned_game = current_user.find_owned_game_by_game_id(params[:game_id])
+      # build note off owned game
+      @note = owned_game.notes.build
+    else
+      redirect_to games_path
+    end
+  end
+  private
+    def note_params
+      params.require(:note).permit(:title, :content, :completed)
+    end
+
 end
