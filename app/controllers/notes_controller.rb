78246f5e5a083d04a9a95396189fa784746892b1
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-13 17:17:04 -0500

    create action builds note off owned game

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,6 +10,20 @@ class NotesController < ApplicationController
       redirect_to games_path
     end
   end
+
+  def create
+    game = Game.find_by(id: params[:game_id])
+
+    @owned_game = current_user.find_owned_game_by_game_id(params[:game_id])
+      @note = @owned_game.notes.build(note_params)
+
+    if @note.save
+      redirect_to game_path(game)
+    else
+      render :new
+    end
+  end
+
   private
     def note_params
       params.require(:note).permit(:title, :content, :completed)
