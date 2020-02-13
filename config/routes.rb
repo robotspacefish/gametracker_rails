8bff58274d982e3eeae27d3313b4e9f960681d70
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-13 14:17:01 -0500

    working on delete action

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     get '/collection', to: 'owned_games#index'
     get '/collection/add_game', to: 'owned_games#new'
     post '/collection', to: 'owned_games#create'
-
+    delete '/collection/:id', to: 'owned_games#destroy'
     get '/account', to: 'users#edit'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
