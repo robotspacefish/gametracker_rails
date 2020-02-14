68ddbc8f0e6909d6a601028f550ff3d71616aeb1
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-13 19:45:35 -0500

    methods to check if a note belongs to a user and get the game

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,12 @@ class Note < ApplicationRecord
   belongs_to :owned_game
 
   scope :sort_newest_first, -> { order(created_at: :desc)}
+
+  def belongs_to_user?(user)
+    self.owned_game.user_id == user.id
+  end
+
+  def game
+    Game.find_by(id: self.owned_game.game_id)
+  end
 end
