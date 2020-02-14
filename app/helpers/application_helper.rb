50b3ade43602312436f3d84a93dab4afdb0d293b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-14 12:29:34 -0500

    human readable datetime helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def human_readable_datetime(datetime)
+    # datetime.
+    datetime.in_time_zone("Eastern Time (US & Canada)").strftime("%A, %d %b %Y %l:%M %p")
+  end
 end
