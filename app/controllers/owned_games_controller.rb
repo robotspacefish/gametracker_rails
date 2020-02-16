459a53edf6949bceaa3920e1ae699554eb3dfc50
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-16 14:41:34 -0500

    delete index action since it's being handled in games controller now

--- app/controllers/owned_games_controller.rb
+++ app/controllers/owned_games_controller.rb
@@ -1,17 +1,6 @@
 class OwnedGamesController < ApplicationController
   before_action :redirect_if_not_logged_in
 
-  def index
-    user = User.find_by(id: params[:user_id])
-    if params[:user_id] && user && is_current_user?(user)
-      # @owned_games = user.games_platforms
-      @owned_games = user.games
-
-    else
-      redirect_to games_path
-    end
-  end
-
   def new
     @game = Game.find_by(id: params[:game_id])
     if !@game
