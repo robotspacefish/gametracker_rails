5f3759bd6b4bef971cd921c5cf3eff780446c302
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-16 19:28:21 -0500

    delete tachyon classes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def display_avatar(user)
     content_tag :div, class: "pa4 tc" do
-      tag.image src: user.get_avatar_url, class: "br-100 pa1 ba b--black-10 h3 w3", alt: "avatar"
+      tag.image src: user.get_avatar_url, height: 40, alt: "avatar"
     end
   end
 end
