700ee5179557c2dae512bddcf5d610c071e4dd86
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-17 13:49:32 -0500

    create display title helper and replace page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     # datetime.
     datetime.in_time_zone("Eastern Time (US & Canada)").strftime("%A, %d %b %Y %l:%M %p")
   end
+
+  def display_title(content)
+    content_tag(:h1, content, class: "display-4 text-center mt-4")
+  end
 end
