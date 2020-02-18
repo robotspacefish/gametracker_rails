4e51b4f873ef7a28a0a548a900209f29b2515f50
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-18 10:33:14 -0500

    delete unused files and platform methods

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -3,15 +3,4 @@ class Platform < ApplicationRecord
 
   has_many :games_platforms
   has_many :games, through: :games_platforms
-
-  def self.setup_platforms
-    platforms = IgdbApi.retrieve_platforms_from_api
-    self.add_platforms_to_db(platforms)
-  end
-
-  def self.add_platforms_to_db(platforms)
-    platforms.each do |platform|
-      self.create(platform)
-    end
-  end
 end
