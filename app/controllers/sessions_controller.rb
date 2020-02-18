c2d228a65a997224df2d2929116d0b1d40d762ea
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-18 13:58:28 -0500

    omniauth action to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,8 +16,29 @@ class SessionsController < ApplicationController
     end
   end
 
+  def omniauth
+    @user = User.find_or_initialize_by(uid: auth[:uid]) do |u|
+      u.username = auth[:info][:email].split('@')[0]
+      u.email = auth[:info][:email]
+      u.password = SecureRandom.hex
+    end
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_games_path(@user)
+    else
+      flash[:message] = "There's already an account with that email address"
+      redirect_to login_path
+    end
+  end
+
   def destroy
     session.delete(:user_id)
     redirect_to root_path
+  end
+
+  private
+  def auth
+    request.env['omniauth.auth']
   end
 end
