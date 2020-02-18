814a7ff6f1fb1e25a419c827b94cf6f7036a0090
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-18 10:38:43 -0500

    delete blanks/commented out code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def human_readable_datetime(datetime)
-    # datetime.
     datetime.in_time_zone("Eastern Time (US & Canada)").strftime("%A, %d %b %Y %l:%M %p")
   end
 
