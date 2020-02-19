7c6300270f799e9fa9194a393d0c514e392df0b0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-18 19:44:54 -0500

    get currently playing games using new status column

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ApplicationRecord
   end
 
   def currently_playing
-    Game.joins(:owned_games).where('user_id = ? AND currently_playing = ?', self.id, true)
+    Game.joins(:owned_games).where('user_id = ? AND status = ?', self.id, :currently_playing)
   end
 
   def find_owned_game_by_game_id(g_id)
