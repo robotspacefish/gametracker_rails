7fbc56cce337192fd4c126894f7e9cab66e25840
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-27 13:31:49 -0500

    change order of day and month

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def human_readable_datetime(datetime)
-    datetime.in_time_zone("Eastern Time (US & Canada)").strftime("%A, %d %b %Y %l:%M %p")
+    datetime.in_time_zone("Eastern Time (US & Canada)").strftime("%A, %b %d %Y %l:%M %p")
   end
 
   def display_title(content)
